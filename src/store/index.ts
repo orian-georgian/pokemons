9b6d8392efbe171b4b31ce313bae398496141f05
Orian Georgian <orian.georgian.external@fujitsu.com>
2024-06-19 16:35:27 +0300

    (state) Implement filters store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 
-import { Pokemon, PokemonStore } from "../types";
+import { Pokemon, PokemonStore, FiltersStore } from "../types";
 
-import { fetchAllPokemons } from "../services/pokemonService";
+import { fetchAllPokemons, fetchTypes } from "../services/pokemonService";
 
 export const usePokemons = create<PokemonStore>((set) => ({
   pokemons: [],
@@ -18,8 +18,40 @@ export const usePokemons = create<PokemonStore>((set) => ({
       console.error("Error fetching Pokemon data:", error);
       set({
         pokemons: [],
-        loading: false,
         filteredPokemons: [],
+        error: "Error fetching Pokemon data",
+      });
+    }
+  },
+  filterPokemons: (search: string, type: string) =>
+    set((state) => ({
+      ...state,
+      filteredPokemons: state.pokemons?.filter(
+        (pokemon) =>
+          pokemon.name.toLowerCase().includes(search.toLowerCase()) &&
+          (!!type ? pokemon.types?.includes(type) : true)
+      ),
+    })),
+}));
+
+export const useFilters = create<FiltersStore>((set) => ({
+  types: [],
+  search: "",
+  type: "",
+  loading: false,
+  error: null,
+  changeSearch: (newSearch) =>
+    set((state) => ({ ...state, search: newSearch })),
+  changeType: (newType) => set((state) => ({ ...state, type: newType })),
+  fetchTypes: async () => {
+    try {
+      set({ loading: true });
+      const types = await fetchTypes();
+      set({ types, loading: false });
+    } catch (error) {
+      console.error("Error fetching Pokemon data:", error);
+      set({
+        types: [],
         error: "Error fetching Pokemon data",
       });
     }
