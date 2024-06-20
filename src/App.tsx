9890e8d0ccea0809147b103fbe0d051c4e4be22d
Orian Georgian <orian.georgian.external@fujitsu.com>
2024-06-20 13:53:03 +0300

    (infrastructure) Deploy app on ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import { usePokemons } from "./store";
 
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
         <Routes>
@@ -25,7 +25,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
