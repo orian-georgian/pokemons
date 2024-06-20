1792d69d134140a2a2393a3d22f407d487b31937
Orian Georgian <orian.georgian.external@fujitsu.com>
2024-06-20 13:06:31 +0300

    (design) Work on mobile design an responsivity of the landing page

--- src/pages/PokemonList/ListItem/index.tsx
+++ src/pages/PokemonList/ListItem/index.tsx
@@ -9,18 +9,16 @@ import { PokemonType } from "../../../types";
 export default function ListItem({ name, imageUrl, types }: Pokemon) {
   return (
     <Link to={`/pokemon/${name}`}>
-      <li className="flex items-center px-10 gap-10 mb-5 border border-gray-200 bg-gradient-to-r from-purple-200 to-orange-200">
+      <li className="flex flex-col sm:flex-row sm:justify-center items-center px-10 sm:gap-10 mb-5 border border-gray-200 bg-gradient-to-r from-purple-200 to-orange-200">
         <Image className="rounded-full w-24 h-24" src={imageUrl} alt={name} />
-        <div>{name}</div>
-        {types.map((type, index) => (
-          <Badge
-            {...(index === 0 ? { className: "ml-auto" } : {})}
-            key={type}
-            type={type as PokemonType}
-          >
-            {type}
-          </Badge>
-        ))}
+        <div className="mb-10 sm:mb-0">{name}</div>
+        <div className="sm:ml-auto sm:mb-0 mb-10 flex gap-3">
+          {types.map((type) => (
+            <Badge key={type} type={type as PokemonType}>
+              {type}
+            </Badge>
+          ))}
+        </div>
       </li>
     </Link>
   );
